84d5360173ceac54126b3763a25d9c8849aa5515
Annie <annie.omori@gmail.com>
2016-09-08 00:38:13 +0900

    First Commit

---
+++ Readme.md
@@ -0,0 +1,30 @@
+# CognitiveEmotionAPI-EmotionWeb-JS
+
+##概要 | Description
+
+[Microsoft Cognitive Services Emotion API](https://www.microsoft.com/cognitive-services/en-us/emotion-api) による 表情判定 Webアプリケーション サンプルです。
+JavaScript (JQuery 3.1.0) を使用しています。
+動作には Emotion API のサブスクリプションが必要です。
+Cognitive Services の各種 API のサブスクリプション申し込み方法は [Microsoft Cognitive Services サブスクリプション準備編](http://qiita.com/annie/items/ba6392b7d1a7647adc4b) をご覧ください。
+**※こちらは アプリケーションサンプルです。ご自身の責任の下ご利用をお願いいたします。**
+
+This is emotion auto-detect sample web app by [Microsoft Cognitive Services Emotion API](https://www.microsoft.com/cognitive-services/en-us/emotion-api).
+It works with JavaScript (JQuery 3.1.0).
+Emotion API Subscription needed to make this work.
+To get Cognitive Services APIs' Subscription Key, see  [How to subscribe Microsoft Cognitive Services APIs](http://qiita.com/annie/items/ba6392b7d1a7647adc4b)
+**THIS IS SAMPLE and please use this for your infomation and on your own responsibility.**
+
+##使用環境 | Environment
+
+Windows 10 (Anniversary Update), Visual Studio Code で作成されています。
+環境構成方法は [Visual Studio Code のインストール](http://qiita.com/annie/items/cd24a22eae7f07536dd1) をご覧ください。
+
+Developed on Windows 10 (Anniversary Update), and Visual Studio Code.
+To get envorpnment, please see [Install Visual Studio Code](http://qiita.com/annie/items/cd24a22eae7f07536dd1).
+
+##利用方法 | How to Use
+ダウンロード後、scripts/script.js を開き、16行目の subscriptionKey に Emotion Api のサブスクリプションキーをコピーして保存します。
+index.html をブラウザで開き、動作を確認できます。
+
+After download bits, open scripts/script.js, paste Emotion API Subscription Key to emotionApiKey on line 16 and save file.
+Open index.html with browser and it should work.
---
+++ scripts/script.js
@@ -0,0 +1,125 @@
+$(function () {
+
+    // 画像を画面に表示
+    var showImage = function () {
+        var imageUrl = $("#imageUrlTextbox").val();
+        if (imageUrl) {
+            $("#ImageToAnalyze").attr("src", imageUrl);
+        }
+    };
+
+    //画像の分析    
+    var getFaceInfo = function () {
+
+        // Emotion API の Subscription Key をセット
+        // お持ちの Subscription Key をコピーしてください
+        var subscriptionKey = "YOUR_SUBSCRIPTION_KEY";
+
+        // 画像 URL をセット
+        var imageUrl = $("#imageUrlTextbox").val();
+
+        // Emotion API 呼び出し URL をセット
+        var webSvcUrl = "https://api.projectoxford.ai/emotion/v1.0/recognize";
+
+        // 画面に表示するメッセージをセット
+        var outputDiv = $("#OutputDiv");
+
+        if(document.getElementById('imageUrlTextbox').value=="")
+        {
+            // 初期設定
+            outputDiv.text("画像のURLを入力してください");
+        }
+        else{
+            // 画像分析中
+            outputDiv.text("分析中...");
+        }
+    
+        // Emotion API を呼び出すためのパラメーターをセットして呼び出し
+        $.ajax({
+            type: "POST",
+            url: webSvcUrl,
+            headers: { "Ocp-Apim-Subscription-Key": subscriptionKey },
+            contentType: "application/json",
+            data: '{ "Url": "' + imageUrl + '" }'
+        }).done(function (data) {
+
+
+            // データが取得出来た場合
+            if (data.length > 0) {
+                // 検出された顔の表示位置を取得
+                var faceRectange = data[0].faceRectangle;
+                var faceWidth = faceRectange.width;
+                var faceHeight = faceRectange.height;
+                var faceLeft = faceRectange.left;
+                var faceTop = faceRectange.top;
+
+                // 画面に描画
+                $("#Rectangle").css("top", faceTop);
+                $("#Rectangle").css("left", faceLeft);
+                $("#Rectangle").css("height", faceHeight);
+                $("#Rectangle").css("width", faceHeight);
+                $("#Rectangle").css("display", "block");
+
+                //小数点6位までを残す関数 (表情スコアの丸めに利用)
+                function floatFormat( number ) {
+                    return Math.round( number * Math.pow( 10 , 6 ) ) / Math.pow( 10 , 6 ) ;
+                }
+
+                // 検出された表情スコアを取得
+                var faceScore = data[0].scores;
+                var faceAnger = floatFormat(faceScore.anger);
+                var faceContempt =  floatFormat(faceScore.contempt);
+                var faceDisgust = floatFormat(faceScore.disgust);
+                var faceFear = floatFormat(faceScore.fear);
+                var faceHappiness = floatFormat(faceScore.happiness);
+                var faceNeutral = floatFormat(faceScore.neutral);
+                var faceSadness = floatFormat(faceScore.sadness);
+                var faceSurprise = floatFormat(faceScore.surprise);                
+
+                // 表情スコアを表示
+                var outputText = "";
+                outputText += "<h3>" + "結果:" + "</h3>";
+                outputText += "怒り　　: " + faceAnger + "<br>";
+                outputText += "軽蔑　　: " + faceContempt + "<br>";
+                outputText += "ムカつき: " + faceDisgust + "<br>";
+                outputText += "恐れ　　: " + faceFear + "<br>";
+                outputText += "喜び　　: " + faceHappiness + "<br>";
+                outputText += "無表情　: " + faceNeutral + "<br>";
+                outputText += "悲しみ　: " + faceSadness + "<br>";
+                outputText += "驚き　　: " + faceSurprise + "<br>";
+
+                outputDiv.html(outputText);
+
+            }
+
+            // データが取得できなかった場合
+            else {
+                outputDiv.text("検出できませんでした");
+            }
+
+        // エラー処理
+        }).fail(function (err) {
+            if(document.getElementById('imageUrlTextbox').value!="")
+            {
+                $("#OutputDiv").text("ERROR!" + err.responseText);
+            }   
+        });
+
+    };
+
+    // 表示するものがない場合
+    var hideMarkers = function () {
+        $("#Rectangle").css("display", "none");
+    };
+
+    // URL が変更された場合（再度分析＆表示)
+    $("#imageUrlTextbox").change(function () {
+        hideMarkers();
+        showImage();
+        getFaceInfo();
+    });
+
+    showImage();    // 画像を画面に表示
+    getFaceInfo();  // 画像を分析
+
+});
